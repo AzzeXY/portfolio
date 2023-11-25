d52dfd9c695e58596a1d0b97d5c144d8c0974153
lucaebert <l.ebert@ebert-mmt.de>
2023-11-25 13:23:45 +0100

    added navbar and photos component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconListComponent } from './ui-components/icon-list/icon-list.component';
+import { NavbarComponent } from './ui-components/navbar/navbar.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
-  declarations: [IconListComponent],
-  exports: [IconListComponent],
-  imports: [CommonModule],
+  declarations: [IconListComponent, NavbarComponent],
+  exports: [IconListComponent, NavbarComponent],
+  imports: [CommonModule, AppRoutingModule],
 })
 export class SharedModule {}
